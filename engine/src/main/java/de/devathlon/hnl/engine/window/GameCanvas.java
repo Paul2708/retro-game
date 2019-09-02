b48babcadcd02b79645d404e21e8aa3e8e82e1b8
Paul2708 <paul.hoger@gmx.net>
2019-09-02 21:27:27 +0200

    Implemented snake and border rendering

--- engine/src/main/java/de/devathlon/hnl/engine/window/GameCanvas.java
+++ engine/src/main/java/de/devathlon/hnl/engine/window/GameCanvas.java
@@ -1,5 +1,9 @@
 package de.devathlon.hnl.engine.window;
 
+import de.devathlon.hnl.core.MapModel;
+import de.devathlon.hnl.core.SnakeModel;
+import de.devathlon.hnl.core.math.Point;
+
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
@@ -12,6 +16,16 @@ public final class GameCanvas extends Canvas {
      */
     private static final int BUFFERS = 3;
 
+    public static final int BLOCK_SIZE = 20;
+
+    public static final int GAP = 2;
+
+    private MapModel mapModel;
+
+    public GameCanvas(MapModel mapModel) {
+        this.mapModel = mapModel;
+    }
+
     public void render() {
         BufferStrategy bufferStrategy = getBufferStrategy();
         if (bufferStrategy == null) {
@@ -23,10 +37,40 @@ public final class GameCanvas extends Canvas {
         graphics.setColor(Color.BLACK);
         graphics.fillRect(0, 0, getWidth(), getHeight());
 
+        // Draw border
+        graphics.setColor(Color.BLUE);
+
+        for (Point point : mapModel.getBorder()) {
+            Point transform = transform(point);
+
+            graphics.fillRect(transform.getX(), transform.getY(), BLOCK_SIZE, BLOCK_SIZE);
+        }
+
+        // Draw snake
         graphics.setColor(Color.RED);
+
+        SnakeModel snakeModel = mapModel.getSnake();
+
+        graphics.fillRect(transform(snakeModel.getHeadPoint()).getX(),
+                transform(snakeModel.getHeadPoint()).getY(), BLOCK_SIZE, BLOCK_SIZE);
+
+        graphics.setColor(Color.WHITE);
+        for (Point point : snakeModel.getBodyPoints()) {
+            Point transform = transform(point);
+
+            graphics.fillRect(transform.getX(), transform.getY(), BLOCK_SIZE, BLOCK_SIZE);
+        }
+
         graphics.drawString("It works!", 100, 100);
 
         graphics.dispose();
         bufferStrategy.show();
+    }
+
+    private Point transform(Point point) {
+        int x = point.getX() == 0 ? 0 : point.getX() * (BLOCK_SIZE + GAP);
+        int y = (int) getSize().getHeight() - BLOCK_SIZE - (BLOCK_SIZE + GAP) * (point.getY());
+
+        return Point.of(x, y);
     }
 }
