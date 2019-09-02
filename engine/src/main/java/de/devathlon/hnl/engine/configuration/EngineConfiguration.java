22cb173e8450151948f8354e6725178523298879
Paul2708 <paul.hoger@gmx.net>
2019-09-02 22:55:31 +0200

    Added getter for width height in blocks

--- engine/src/main/java/de/devathlon/hnl/engine/configuration/EngineConfiguration.java
+++ engine/src/main/java/de/devathlon/hnl/engine/configuration/EngineConfiguration.java
@@ -6,16 +6,32 @@ import java.awt.Dimension;
 
 public class EngineConfiguration {
 
+    private final int widthInBlocks;
+    private final int heightInBlocks;
+
     private final Dimension dimension;
+
     private final int fps;
 
     public EngineConfiguration(int widthBlocks, int heightBlocks, int fps) {
+        this.widthInBlocks = widthBlocks;
+        this.heightInBlocks = heightBlocks;
+
         this.dimension = new Dimension(transform(widthBlocks), transform(heightBlocks));
+
         this.fps = fps;
     }
 
     private int transform(int blocks) {
         return blocks * GameCanvas.BLOCK_SIZE + (blocks - 1) * GameCanvas.GAP;
+    }
+
+    public int getWidthInBlocks() {
+        return widthInBlocks;
+    }
+
+    public int getHeightInBlocks() {
+        return heightInBlocks;
     }
 
     public Dimension getDimension() {
