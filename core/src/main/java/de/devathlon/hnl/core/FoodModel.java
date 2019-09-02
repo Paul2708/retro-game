e50db19508fdb01f756d7b0a5511cc7eff24194b
Paul2708 <paul.hoger@gmx.net>
2019-09-02 23:16:23 +0200

    Added color to food and draw food

--- core/src/main/java/de/devathlon/hnl/core/FoodModel.java
+++ core/src/main/java/de/devathlon/hnl/core/FoodModel.java
@@ -2,7 +2,11 @@ package de.devathlon.hnl.core;
 
 import de.devathlon.hnl.core.math.Point;
 
+import java.awt.Color;
+
 public interface FoodModel {
 
     Point getLocation();
+
+    Color getColor();
 }
