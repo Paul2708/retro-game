138215e8973b4695e9ce8e196fec00a0e2c854b6
Paul2708 <paul.hoger@gmx.net>
2019-09-02 18:21:03 +0200

    Added default engine configuration

---
+++ engine/src/main/java/de/devathlon/hnl/engine/configuration/DefaultEngineConfiguration.java
@@ -0,0 +1,10 @@
+package de.devathlon.hnl.engine.configuration;
+
+import java.awt.Dimension;
+
+public class DefaultEngineConfiguration extends EngineConfiguration {
+
+    public DefaultEngineConfiguration() {
+        super(new Dimension(480, 640), 30);
+    }
+}
