3c9dcb9b4b72889c9b3c2622dabae37fd73c062b
Paul2708 <paul.hoger@gmx.net>
2019-09-02 21:23:24 +0200

    Changed configuration from size in pixel to size in blocks

--- engine/src/main/java/de/devathlon/hnl/engine/configuration/EngineConfiguration.java
+++ engine/src/main/java/de/devathlon/hnl/engine/configuration/EngineConfiguration.java
@@ -1,15 +1,21 @@
 package de.devathlon.hnl.engine.configuration;
 
-import java.awt.*;
+import de.devathlon.hnl.engine.window.GameCanvas;
+
+import java.awt.Dimension;
 
 public class EngineConfiguration {
 
     private final Dimension dimension;
     private final int fps;
 
-    public EngineConfiguration(Dimension dimension, int fps) {
-        this.dimension = dimension;
+    public EngineConfiguration(int widthBlocks, int heightBlocks, int fps) {
+        this.dimension = new Dimension(transform(widthBlocks), transform(heightBlocks));
         this.fps = fps;
+    }
+
+    private int transform(int blocks) {
+        return blocks * GameCanvas.BLOCK_SIZE + (blocks - 1) * GameCanvas.GAP;
     }
 
     public Dimension getDimension() {
