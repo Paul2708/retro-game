349a9529dd6a78099197748ce7dcf57360ab0650
Paul2708 <paul.hoger@gmx.net>
2019-09-03 14:49:46 +0200

    Added configuration documentation

--- engine/src/main/java/de/devathlon/hnl/engine/configuration/DefaultEngineConfiguration.java
+++ engine/src/main/java/de/devathlon/hnl/engine/configuration/DefaultEngineConfiguration.java
@@ -1,7 +1,16 @@
 package de.devathlon.hnl.engine.configuration;
 
+/**
+ * This {@link EngineConfiguration} can be used as default engine configuration.
+ * It creates a small game map with 30 FPS.
+ *
+ * @author Paul2708
+ */
 public class DefaultEngineConfiguration extends EngineConfiguration {
 
+    /**
+     * Create a new default configuration with a small map and 30 fps.
+     */
     public DefaultEngineConfiguration() {
         super(20, 35, 30);
     }
--- engine/src/main/java/de/devathlon/hnl/engine/configuration/EngineConfiguration.java
+++ engine/src/main/java/de/devathlon/hnl/engine/configuration/EngineConfiguration.java
@@ -4,6 +4,14 @@ import de.devathlon.hnl.engine.window.GameCanvas;
 
 import java.awt.Dimension;
 
+/**
+ * This class holds configuration settings for {@link de.devathlon.hnl.engine.GameEngine}.
+ * They can be applied before starting the engine.
+ * <p>
+ * It contains information like game size and fps.
+ *
+ * @author Paul2708
+ */
 public class EngineConfiguration {
 
     private final int widthInBlocks;
@@ -13,6 +21,15 @@ public class EngineConfiguration {
 
     private final int fps;
 
+    /**
+     * Create a new engine configuration.
+     * The real {@link Dimension} will be calculated based on given block size and {@link GameCanvas#BLOCK_SIZE}
+     * and {@link GameCanvas#GAP}.
+     *
+     * @param widthBlocks  screen width in game blocks
+     * @param heightBlocks screen height in game blocks
+     * @param fps          frames per seconds, the screen will update <code>1/fps</code> seconds
+     */
     public EngineConfiguration(int widthBlocks, int heightBlocks, int fps) {
         this.widthInBlocks = widthBlocks;
         this.heightInBlocks = heightBlocks;
@@ -22,22 +39,50 @@ public class EngineConfiguration {
         this.fps = fps;
     }
 
+    /**
+     * Transform given amount in blocks to real screen size.
+     * It's based on {@link GameCanvas#GAP} and {@link GameCanvas#BLOCK_SIZE}.
+     *
+     * @param blocks amount of blocks
+     * @return transformed real screen size
+     */
     private int transform(int blocks) {
         return blocks * GameCanvas.BLOCK_SIZE + (blocks - 1) * GameCanvas.GAP;
     }
 
+    /**
+     * Get the width in blocks.
+     *
+     * @return width in blocks
+     */
     public int getWidthInBlocks() {
         return widthInBlocks;
     }
 
+    /**
+     * Get the height in blocks.
+     *
+     * @return height in blocks
+     */
     public int getHeightInBlocks() {
         return heightInBlocks;
     }
 
+    /**
+     * Get the calculated dimension based on size in blocks.
+     *
+     * @return calculated dimension
+     */
     public Dimension getDimension() {
         return dimension;
     }
 
+    /**
+     * Get the frames per second.
+     * The screen will update <code>1/fps</code> seconds.
+     *
+     * @return fps
+     */
     public int getFps() {
         return fps;
     }
