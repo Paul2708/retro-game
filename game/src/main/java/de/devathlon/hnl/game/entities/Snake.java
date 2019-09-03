0c9ff119239cf915645e663ff065fc365be3c023
Leon-DE <leon@carmincke.info>
2019-09-03 17:21:35 +0200

    border update and code cleanup

--- game/src/main/java/de/devathlon/hnl/game/entities/Snake.java
+++ game/src/main/java/de/devathlon/hnl/game/entities/Snake.java
@@ -1,16 +1,24 @@
 package de.devathlon.hnl.game.entities;
 
+import de.devathlon.hnl.core.FoodModel;
 import de.devathlon.hnl.core.SnakeModel;
 import de.devathlon.hnl.core.math.Point;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Snake implements SnakeModel {
 
     private Point headPoint;
     private ArrayList<Point> bodyPoints;
 
+    private long speed;
+    private boolean invincible;
+
     public Snake() {
+        this.speed = 100;
+        this.invincible = false;
+
         bodyPoints = new ArrayList<>();
         // generate snake at 10;10 with 4 body points
         headPoint = Point.of(10, 10);
@@ -28,5 +36,58 @@ public class Snake implements SnakeModel {
     @Override
     public ArrayList<Point> getBodyPoints() {
         return bodyPoints;
+    }
+
+    public void setSpeed(long speed) {
+        this.speed = speed;
+    }
+
+    public long getSpeed() {
+        return speed;
+    }
+
+    public void setInvincible(boolean invincible) {
+        this.invincible = invincible;
+    }
+
+    public boolean isInvincible() {
+        return invincible;
+    }
+
+    public boolean collisionWithBody() {
+        if (isInvincible()) return false;
+        for (Point point : bodyPoints) {
+            if (point.getX() == headPoint.getX() && point.getY() == headPoint.getY()) return true;
+        }
+        return false;
+    }
+
+    public boolean collisionWithBorder(List<Point> borderPoints) {
+        for (Point borderPoint : borderPoints) {
+            if (headPoint.getX() == borderPoint.getX() && headPoint.getY() == borderPoint.getY())
+                return true;
+        }
+        return false;
+    }
+
+    public FoodModel contactWithFood(List<FoodModel> foodList) {
+        for (FoodModel food : foodList) {
+            if (food.getLocation().getX() == headPoint.getX() && food.getLocation().getY() == headPoint.getY())
+                return food;
+        }
+
+        return null;
+    }
+
+    public void updateBody(int oldHeadX, int oldHeadY) {
+        int x = oldHeadX;
+        int y = oldHeadY;
+        for (Point point : bodyPoints) {
+            int tempX = point.getX();
+            int tempY = point.getY();
+            point.set(x, y);
+            x = tempX;
+            y = tempY;
+        }
     }
 }
