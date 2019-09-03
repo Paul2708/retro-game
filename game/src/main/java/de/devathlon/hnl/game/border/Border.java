c8daa161a1add5aad93d6f8679f8b5e5ecef1398
Leon-DE <leon@carmincke.info>
2019-09-03 15:20:53 +0200

    new border system

---
+++ game/src/main/java/de/devathlon/hnl/game/border/Border.java
@@ -0,0 +1,10 @@
+package de.devathlon.hnl.game.border;
+
+import de.devathlon.hnl.core.math.Point;
+
+import java.util.Collection;
+
+public interface Border {
+
+    Collection<Point> getBorderPoints();
+}
